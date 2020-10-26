70dc26615e319c01abdd17632ee6c11a99830497
dannyunico <danielbermudezreyes@gmail.com>
2020-10-26 10:38:33 -0400

    pequeños ajustes

--- wp-content/themes/Estudio-Digital/partials/comentarios.php
+++ wp-content/themes/Estudio-Digital/partials/comentarios.php
@@ -3,7 +3,7 @@
     <div class="main-comentarios" style="background-image: url('<?php echo get_template_directory_uri();?>/assets/img/Enmascarar grupo 122.png'); background-repeat: no-repeat; background-size: 50%;
     background-position-x: 10%; height: 600px; width: 100%;">
       <div class="main-comentarios__content"></div>
-      <div class="main-comentarios__content">
+      <div class="main-comentarios__content container">
         <div class="subtitle-general subtitle-general-secun">
           <p>Comentarios</p>
         </div>
